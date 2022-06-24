c2e133ee2c726bf4e695a25f63923adfe7eb867d
PhKhang <84757707+PhKhang@users.noreply.github.com>
2022-06-24 13:58:32 +0700

    Add files via upload

---
+++ selectnewnum.js
@@ -0,0 +1,25 @@
+document.getElementById('nut').onclick = function () {
+    randint = getRandomInt();
+    randurl = "https://comment.nld.com.vn/TraDiem/Lop10/TraDiemTHPT.aspx?SoBD=" + randint;
+    console.log(randurl);
+    document.getElementById('frame').src = randurl;
+
+};
+
+document.getElementById('frame').onload = () => {
+    console.log('loaded');
+    yeet();
+  }
+
+function getRandomInt() {
+    min = 100000;
+    max = 200000;
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+  }
+
+function yeet(){
+    const elements = document.getElementsByClassName("bgxemdiemthilop10 frm-search-web");
+    while(elements.length > 0){
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+}
