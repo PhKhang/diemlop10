1bf7365b72a42b0d69f4e0082ca6eb551c91ce34
PhKhang <84757707+PhKhang@users.noreply.github.com>
2022-06-25 09:11:01 +0700

    Update selectnewnum.js

--- selectnewnum.js
+++ selectnewnum.js
@@ -15,6 +15,20 @@ document.getElementById('frame').onload = () => {
 function getRandomInt() {
     min = 100000;
     max = 200000;
+    seed = Math.floor(Math.random() * (3 - 1) + 1);
+    switch (seed) {
+  case 1:
+    return Math.floor(Math.random() * (96013 - 90000) + 90000);
+    break;
+  case 2:
+    return Math.floor(Math.random() * (9571 - 9000) + 9000);
+    break;
+  case 3:
+    return Math.floor(Math.random() * (186191 - 100000) + 100000);
+    break;
+  default:
+    // code
+}
     return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
   }
 
